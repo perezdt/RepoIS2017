11b51aa75ae6a4e31bfd5db7a6d447a6cf7c6ac7
perezdt <perezdt@gmail.com>
2017-11-09 20:20:32 -0300

    Agrego Resta

--- Calculadora.java
+++ Calculadora.java
@@ -5,6 +5,16 @@ public class Calculadora{
 				int op1 = Integer.ParseInt(args[1]);
 				int op2 = Integer.ParseInt(args[2]);
 				
+				if("+".equals(args[0])){
+					System.out.println(op1+op2);
+				}
+				else if ("-".equals(args[0])){
+					System.out.println(op1-op2);
+				}
+				else{
+					System.out.println(args[0] + ": operacion desconocida");
+					
+				
 				System.out.println(op1 + op2);
 			}
 			catch (Exception ex){
